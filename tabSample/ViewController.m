b91c4e8beea59f01eae6a1c82d832a7b141f7c84
KawamataKouhei <kawamata@basicinc.jp>
2014-10-31 20:31:20 +0900

    Initial Commit

---
+++ tabSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tabSample
+//
+//  Created by 川又康平 on 2014/10/31.
+//  Copyright (c) 2014年 mmusasabi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ tabSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tabSample
+//
+//  Created by 川又康平 on 2014/10/31.
+//  Copyright (c) 2014年 mmusasabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
